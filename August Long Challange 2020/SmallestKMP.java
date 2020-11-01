6178de38b424834c35fd89ba9778a6fe88d7155d
Sona Shaw <51262648+SonaShaw@users.noreply.github.com>
2020-11-01 19:43:03 +0530

    Smallest KMP Solution

--- August Long Challange 2020/SmallestKMP.java
+++ August Long Challange 2020/SmallestKMP.java
@@ -1,48 +1,62 @@
-package codeChef;
+package augLongChallenge2020;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class SmallestKMP {
-
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int t = sc.nextInt();
-		while(t-->0) {
-			int a[] = new int[26];
-			int b[] = new int[26];
-			String s = sc.next();
-			String p = sc.next();
-			
-			for(int i=0; i<s.length(); i++) {
-				char ch = s.charAt(i);
-				int index = (int)ch;
-				a[index-97]++;
-			}
-			for(int i=0; i<p.length(); i++) {
-				char ch = p.charAt(i);
-				int index = (int)ch;
-				a[index-97]--;
-				b[index-97]++;
-			}
-			char ch = p.charAt(0);
-			int ind = (int)ch - 97;
-			String result = "";
-			for(int i=0; i<=ind; i++) {
-				int cnt = a[i];
-				for(int j=0; j<cnt; j++) {
-					result = result+(char)(i+97);
-				}
-			}
-			result = result+p;
-			for(int i=ind+1; i<a.length; i++) {
-				int cnt = a[i];
-				for(int j=0; j<cnt; j++) {
-					result = result+(char)(i+97);
-				}
-			}
-			System.out.println(result);
-		}
-
-	}
-
+    public static void main(String args[]) {
+	Scanner sc=new Scanner(System.in);
+        int T=sc.nextInt();
+        while(T>0) {
+            String s=sc.next();
+            String p=sc.next();
+            int countS[]=new int[26];
+            for(int i=0; i<s.length(); i++)
+                countS[s.charAt(i)-'a']++;
+            for(int i=0; i<p.length(); i++)
+                countS[p.charAt(i)-'a']--;
+                                   
+            char iniChar='a';
+            ArrayList<Character>ans=new ArrayList<>();
+            for(int i=0; i<26; i++, iniChar++) {
+                if(countS[i]>0) {
+                    if(iniChar<p.charAt(0)) {
+                        for(int j=0; j<countS[i]; j++)
+                            ans.add(iniChar);
+                    } 
+                    else if(iniChar==p.charAt(0)) {
+                        String curr="";
+                        for(int j=0; j<countS[i]; j++)
+                            curr+=iniChar;
+                        if((curr+p).compareTo(p+curr)<0) {
+                            for(int j=0; j<curr.length(); j++)
+                                ans.add(curr.charAt(j));
+                            for(int j=0; j<p.length(); j++)
+                                ans.add(p.charAt(j));
+                        } 
+                        else {
+                            for(int j=0; j<p.length(); j++)
+                                ans.add(p.charAt(j));
+                            for(int j=0; j<curr.length(); j++)
+                                ans.add(curr.charAt(j));
+                        }
+                    }
+                    else {
+                        for(int j=0; j<countS[i]; j++)
+                            ans.add(iniChar);
+                    }
+                }
+                else if(iniChar==p.charAt(0)) {
+                    for(int j=0; j<p.length(); j++) {
+                        ans.add(p.charAt(j));
+                    }
+                }
+            }
+            
+            for(int x=0; x<ans.size(); x++)
+                System.out.print(ans.get(x));
+            System.out.println();
+            
+            T--;
+        }
+    }
 }
